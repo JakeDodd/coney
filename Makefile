e193844ac8bb15de30b7d4ec43ce325a82f16a66
Jacob Dodd <doddj@duq.edu>
2025-06-19 11:05:00 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,18 @@
+CC=gcc
+CFLAGS=-g -Wall -Wextra -pedantic -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+DEPS = 
+OBJ = src/main.o
+
+BDIR=build
+
+%.o: %.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+$(BDIR)/main: $(OBJ)
+	if [ ! -d build ]; then mkdir build; fi
+	$(CC) $^ -o $@ $(CFLAGS)
+
+.PHONY: clean
+
+clean:
+	rm $(OBJ)
---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <raylib.h>
+
+typedef struct Circle {
+  int x;
+  int y;
+  int speed;
+} Circle;
+
+void updateCirclePosition(Circle *circle) {
+  if (IsKeyDown(KEY_W)) {
+    circle->y -= circle->speed;
+  }
+  if (IsKeyDown(KEY_A)) {
+    circle->x -= circle->speed;
+  }
+  if (IsKeyDown(KEY_S)) {
+    circle->y += circle->speed;
+  }
+  if (IsKeyDown(KEY_D)) {
+    circle->x += circle->speed;
+  }
+}
+
+int main() {
+
+  // Init window
+  InitWindow(1000, 666, "myWindow");
+  SetTargetFPS(60);
+
+  Circle player = {.x = 100, .y = 100, .speed = 5};
+
+  // game loop
+  while (!WindowShouldClose()) {
+
+    // All drawing happens below
+    BeginDrawing();
+
+    ClearBackground(SKYBLUE);
+
+    updateCirclePosition(&player);
+    DrawCircle(player.x, player.y, 20, RAYWHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
